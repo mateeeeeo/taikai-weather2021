9d59832cc0f133b3125ada7ff3e248cbe88c38aa
rares-tudor <rarestudor04@gmail.com>
2021-07-19 17:16:48 +0300

    started system responsible for fetching forecastsS

--- src/helpers/DateFormat.ts
+++ src/helpers/DateFormat.ts
@@ -20,3 +20,23 @@ export function inputFormat(date: Date): string {
 
     return `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
 }
+
+export function revformat(m: string, d: number, y: number): Date {
+    var m_no: number;
+    switch (m) {
+        case ("January"): m_no = 1; break;
+        case ("February"): m_no = 2; break;
+        case ("March"): m_no = 3; break;
+        case ("April"): m_no = 4; break;
+        case ("May"): m_no = 5; break;
+        case ("June"): m_no = 6; break;
+        case ("July"): m_no = 7; break;
+        case ("August"): m_no = 8; break;
+        case ("September"): m_no = 9; break;
+        case ("October"): m_no = 10; break;
+        case ("November"): m_no = 11; break;
+        case ("December"): m_no = 12; break;
+        default: m_no = 0; break;
+    };
+    return new Date(m_no, d, y);
+}
