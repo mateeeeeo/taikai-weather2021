45d5162b2e1d63cdfd1814304deaff348fb2bdfc
rares-tudor <rarestudor04@gmail.com>
2021-07-21 15:35:51 +0300

    s

--- src/FetchForecasts.ts
+++ src/FetchForecasts.ts
@@ -27,59 +27,66 @@ type Location = {
 export let locations: Array<Location>;
 const path = "C:\\Users\\Administrator\\Documents\\taikai-weather2021\\src\\assets\\forecasts.txt"
 
-let actlocation: Location;
-let actforecast: Forecast;
+export function fetchForecasts(range:number) { //The forecasts will be saved in global variable locations
+  let actforecast: Forecast;
+  let actlocation: Location;
+  const actDay = new Date().getDay();
+  let inrange: boolean;
 
-const reader = readline.createInterface(fs.createReadStream(path));
-reader.on("line", (l: string) => {
-  const s = l.split(' ');
-  if (s[0].endsWith(',')) {
-    actlocation = { name: s[0].substring(0, s[0].length - 1), state: s[1].substring(0, s[1].length - 1) }
-
-    const d: number = parseInt(s[3].substring(0, s[3].length - 1)); const yr: number = parseInt(s[4])
-    const ndate: Date = revformat(s[2], d, yr)
-    actforecast.date = ndate;
-  }
-  else
-  {
-    switch (s[0].toLowerCase())
-    {
-      case "pressure:":
-        actforecast.weather_info.pressure = parseFloat(s[1]);
-        break;
-      case "temperature:":
-        actforecast.weather_info.temp = parseFloat(s[1]);
-        break;
-      case "humidity:":
-        actforecast.weather_info.humidity = parseFloat(s[1]);
-        break;
-      case "condition:":
-        let aux: string="";
-        for (let i = 1; i < s.length; ++i)
-          aux += s[i]; aux += " ";
-        actforecast.weather_info.condition = aux;
-        break;
-      case "wind:":
-        if (s[1] == "Unknown") actforecast.weather_info.wind = ["N/A",-1];
-        else actforecast.weather_info.wind = [s[1], parseInt(s[3])];
-        break;
-      case "chance":
-        // this is also the end of the buffer which is why we'll be checking for duplicates
-        actforecast.weather_info.rain_chance = parseInt(s[3]);
-
-        let match: boolean = false;
-        locations.forEach(location => {
-          if (location.name.toLowerCase() == actlocation.name.toLowerCase()) // identical match
-          {
-            match = true;
-            location.forecasts?.push(actforecast); // we simply add another forecast to the existing batch
-            return;
-          }
-        });
-        if (match) break;
-        actlocation.forecasts?.push(actforecast); // we create a new location if that is not the case
-        locations.push(actlocation);
-        break;
+  const reader = readline.createInterface(fs.createReadStream(path));
+  reader.on("line", (l: string) => {
+    const s = l.split(' ');
+    if (s[0].endsWith(',')) {
+      inrange = true;
+      const d: number = parseInt(s[3].substring(0, s[3].length - 1));
+      if (Math.abs(actDay - d) <= range) {
+        const yr: number = parseInt(s[4])
+        const ndate: Date = revformat(s[2], d, yr)
+      
+        actlocation = { name: s[0].substring(0, s[0].length - 1), state: s[1].substring(0, s[1].length - 1) }
+        actforecast.date = ndate;
+      }
+    
     }
-  }
-})
+    else {
+      if (inrange) switch (s[0].toLowerCase()) {
+          case "pressure:":
+            actforecast.weather_info.pressure = parseFloat(s[1]);
+            break;
+          case "temperature:":
+            actforecast.weather_info.temp = parseFloat(s[1]);
+            break;
+          case "humidity:":
+            actforecast.weather_info.humidity = parseFloat(s[1]);
+            break;
+          case "condition:":
+            let aux: string = "";
+            for (let i = 1; i < s.length; ++i)
+              aux += s[i]; aux += " ";
+            actforecast.weather_info.condition = aux;
+            break;
+          case "wind:":
+            if (s[1] == "Unknown") actforecast.weather_info.wind = ["N/A", -1];
+            else actforecast.weather_info.wind = [s[1], parseInt(s[3])];
+            break;
+          case "chance":
+            // this is also the end of the buffer which is why we'll be checking for duplicates
+            actforecast.weather_info.rain_chance = parseInt(s[3]);
+
+            let match: boolean = false;
+            locations.forEach(location => {
+              if (location.name.toLowerCase() == actlocation.name.toLowerCase()) // identical match
+              {
+                match = true;
+                location.forecasts?.push(actforecast); // we simply add another forecast to the existing batch
+                return;
+              }
+            });
+            if (match) break;
+            actlocation.forecasts?.push(actforecast); // we create a new location if that is not the case
+            locations.push(actlocation);
+            break;
+        }
+    }
+  })
+}
